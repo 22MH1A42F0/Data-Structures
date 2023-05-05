52f541f1cbd10d750e3438790035a926cb51befa
22MH1A42F0 <119723543+22MH1A42F0@users.noreply.github.com>
2023-05-05 12:17:12 +0530

    Add files via upload

---
+++ bubble sort char.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void bubble_sort(char *arr,int n)
+{
+   int i,j,temp;
+   for(i=0;i<n;i++)
+   {
+   	 for(j=0;j<n-1;j++)
+   	 {
+   	  if(arr[j]>arr[j+1])
+   	  {
+       temp=arr[j];
+       arr[j]=arr[j+1];
+       arr[j+1]=temp;
+      }
+     }
+   } 
+}
+int main()
+{
+	int n;
+    scanf("%d",&n);
+	char arr[n];
+	scanf("%s",arr);
+	bubble_sort(arr,n);
+     printf("%s",arr);
+	    	
+}
